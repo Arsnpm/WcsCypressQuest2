d0fb66ff0c34ce7749c378b125ff18177398f12b
Arsnpm <pouget.arsene@gmail.com>
2022-04-06 15:18:41 +0200

    initial commit

---
+++ cypress/integration/backmarket/authentication-tests-preprod.spec.js
@@ -0,0 +1,91 @@
+import { faker } from '@faker-js/faker';
+
+let userData = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(20)
+};
+ 
+describe('Authentication Tests suite preprod.backmarket', () => {
+    beforeEach(() => {
+        cy.visit('/register')
+    })
+    it('should authenticate user with valid credentials', () => {
+        //On accepte les cookies 
+        cy.get('[data-qa="accept-cta"]').click();
+       
+        //le prénom peut être un string de 1 char ou bien encore 1 nombre mais ne doit pas être vide
+        cy.get('#firstName').should('not.have.css', 'border-color', 'rgb(169, 15, 20)').type(userData.firstName).should('have.value', userData.firstName);
+
+        //le nom peut être un string de 1 char ou bien encore 1 nombre mais ne doit pas être vide
+        cy.get('#lastName').should('not.have.css', 'border-color', 'rgb(169, 15, 20)').type(userData.lastName).should('have.value', userData.lastName);
+
+        //l'email peut être bizarre genre 1@5.51
+        //erreur annoncée: Saisissez une adresse e-mail valide.
+        cy.get('#signup-email').should('not.have.css', 'border-color', 'rgb(169, 15, 20)').type(userData.email).should('have.value', userData.email);
+
+        //le mot de passe doit faire une certaine longueur
+        //erreur annoncée: Au moins 8 caractères, dont 1 majuscule, 1 minuscule et 1 chiffre.
+        cy.get('#signup-password').should('not.have.css', 'border-color', 'rgb(169, 15, 20)').type(userData.password);
+
+        cy.get('[data-qa="signup-submit-button"]').click()
+        cy.wait(100);
+
+        //Assertions
+        cy.url().should('eq', 'https://preprod.backmarket.fr/dashboard/orders');
+        cy.contains('Mon profil').should('be.visible');
+        cy.get('h2').contains('Mes commandes');
+        cy.get('[data-qa="dashboard-navigation-profil"]').should('exist');
+    });
+
+    it('should display error message for invalid password', () => {
+        
+        cy.get('[data-qa="accept-cta"]').click();
+
+        cy.get('#firstName').type(userData.firstName).should('have.value', userData.firstName);
+        cy.get('#lastName').type(userData.lastName).should('have.value', userData.lastName);
+        cy.get('#signup-email').type(userData.email).should('have.value', userData.email);
+        cy.get('#signup-password').type("1234");
+        //tester la phrase d'erreur
+        cy.contains("Au moins 8 caractères, dont 1 majuscule, 1 minuscule et 1 chiffre. Parce qu'on sait jamais.").should('exist');
+        cy.get('[data-qa="signup-submit-button"]').click();     
+    });
+    it('should display error message for invalid email', () => {
+        
+        cy.get('[data-qa="accept-cta"]').click();
+
+        cy.get('#firstName').type(userData.firstName).should('have.value', userData.firstName);
+        cy.get('#lastName').type(userData.lastName).should('have.value', userData.lastName);
+        cy.get('#signup-email').type("gdfgdfg@555");
+        //tester la phrase d'erreur
+        cy.get('#signup-password').type(userData.password).should('have.value', userData.password);
+
+        cy.get('[data-qa="signup-submit-button"]').click();  
+        cy.contains("Saisissez une adresse e-mail valide.").should('exist'); 
+    });
+    it('should display error message for invalid fields', () => {   
+
+        cy.get('[data-qa="accept-cta"]').click();        
+        cy.get('[data-qa="signup-submit-button"]').click();
+
+        cy.get('#firstName').should('have.css', 'border-color', 'rgb(169, 15, 20)');
+        cy.get('#lastName').should('have.css', 'border-color', 'rgb(169, 15, 20)');
+        cy.get('#signup-email').should('have.css', 'border-color', 'rgb(169, 15, 20)');
+        cy.get('#signup-password').should('have.css', 'border-color', 'rgb(169, 15, 20)');
+        
+        cy.get('[data-test="signup-component"]').contains("Le champ \"Prénom\" est obligatoire").should('exist'); 
+        cy.get('[data-test="signup-component"]').contains("Le champ \"Nom\" est obligatoire").should('exist'); 
+        cy.get('[data-test="signup-component"]').contains("Le champ \"Email\" est obligatoire").should('exist'); 
+        cy.get('[data-test="signup-component"]').contains("Le champ mot de passe est obligatoire").should('exist');        
+    });
+    it('should login user with valid credentials', () => {
+
+        cy.get('[data-qa="accept-cta"]').click();
+        cy.get('#signin-email').should('not.have.css', 'border-color', 'rgb(169, 15, 20)').type(userData.email).should('have.value', userData.email);
+        cy.get('#signin-password').should('not.have.css', 'border-color', 'rgb(169, 15, 20)').type(userData.password);
+
+        cy.get('[data-qa="signin-submit-button"]').click();
+        cy.url().should('eq', 'https://preprod.backmarket.fr/dashboard/orders');
+    });
+})
